9ca968dc3b6a33f41e1af62c2fcc29f5af7a4967
NTS-AkshayBokhare <akshay.bokhare@neutrinotechlabs.com>
2024-12-05 12:22:00 +0530

    Two way binding

---
+++ src/app/home-component/home-component.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-home-component',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './home-component.component.html',
+  styleUrl: './home-component.component.css'
+})
+export class HomeComponentComponent {
+
+  StaticInput:string = "Satic two way binding";
+  DynamicInput: string = "";
+}
